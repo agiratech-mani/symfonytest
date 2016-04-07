033378a22affcd8466ce2fdc31a79155a9c3919a
mani-agiratech <manikandan@agiratech.com>
2016-04-07 19:34:05 +0530

    Project Add

---
+++ src/AppBundle/Controller/ProjectsController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ProjectsController extends Controller
+{
+
+    public function indexAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('welcome/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
+        ]);
+       /* $number = rand(0, 100);
+
+        return new Response(
+            '<html><body>Lucky number: '.$number.'</body></html>'
+        );*/
+    }
+    public function addAction()
+    {
+        // replace this example code with whatever you need
+        return $this->render('projects/add.html.twig');
+    }
+}
---
+++ src/AppBundle/Entity/Projects.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace AppBundle\Entity;
+
+/**
+ * Projects
+ */
+class Projects
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var string
+     */
+    private $price;
+
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Projects
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set price
+     *
+     * @param string $price
+     *
+     * @return Projects
+     */
+    public function setPrice($price)
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    /**
+     * Get price
+     *
+     * @return string
+     */
+    public function getPrice()
+    {
+        return $this->price;
+    }
+}
